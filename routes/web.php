176ab511a2b14368c457543491704b7ff25d2c21
lubnaOm <lubnao115@gmail.com>
2020-02-03 00:28:53 +0300

    dashboard page

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('annou');
+    return view('dashboard');
 });
+
 Route::resource('/Announcaments','AnnouncamentsController');
+
 
 Auth::routes();
 
